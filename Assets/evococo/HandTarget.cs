04be6829dbc3f00197b8fa9c72b1b02b491d3b8a
raiganja55-boop <raiganja55@gmail.com>
2026-07-02 23:44:41 +0700

    evo

---
+++ Assets/evococo/HandTarget.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HandTarget : MonoBehaviour
+{
+    public Transform hand;      // มือผู้เล่น
+    public Renderer target;     // Renderer ของ Sphere
+
+    public float hitDistance = 0.10f;
+
+    void Update()
+    {
+        float distance = Vector3.Distance(hand.position, transform.position);
+
+        if (distance <= hitDistance)
+        {
+            target.material.color = Color.green;
+        }
+        else
+        {
+            target.material.color = Color.red;
+        }
+    }
+}
