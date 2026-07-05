aa8ec747b70866375403cd163d8f5b7d832ff35d
TheRealPru <TheRealPru@users.noreply.github.com>
2026-07-05 19:38:43 +0700

    Add Building Model + Config punch distance

---
+++ Assets/Scripts/RageRoom/GameManger.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public float timer = 120f;
+    public int objectsRemaining;
+
+    bool gameEnded = false;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        objectsRemaining = FindObjectsOfType<DestructibleObject>().Length;
+    }
+    void Update()
+    {
+        if (gameEnded) return;
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0 || objectsRemaining <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    public void ObjectDestroyed()
+    {
+        objectsRemaining--;
+    }
+
+    void EndGame()
+    {
+        gameEnded = true;
+        UnityEngine.Debug.Log("Game Ended");
+    }
+}
