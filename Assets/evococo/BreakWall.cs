9b1ff02defb92babd75591fcc881c93dcdea15fc
raiganja55-boop <raiganja55@gmail.com>
2026-07-06 21:33:23 +0700

    ev

--- Assets/evococo/BreakWall.cs
+++ Assets/evococo/BreakWall.cs
@@ -4,11 +4,20 @@ public class BreakWall : MonoBehaviour
 {
     public GameObject brokenWall;
 
+    private bool broken = false;
+
     private void OnTriggerEnter(Collider other)
     {
+        if (broken) return;
+
         if (other.CompareTag("Glove"))
         {
-            Instantiate(brokenWall, transform.position, transform.rotation);
+            broken = true;
+
+            Instantiate(brokenWall,
+                        transform.position,
+                        transform.rotation);
+
             Destroy(gameObject);
         }
     }
